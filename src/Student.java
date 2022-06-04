0343a5552640a446137770712ab39c9e7a016a2c
= <=>
2022-06-04 13:45:22 +0500

    Added Student class and methods

--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,19 @@
+import javax.swing.*;
+
 public class Student {
     private String studentName;
     private String regNo;
     private String batch;
-    private float c_gpa;
-    private float s_gpa;
+    private double c_gpa;
+    private double s_gpa;
     private String phoneNumber;
     private String email;
+    private String section;
 
-    public void Student(String student_name, String reg_no , String Batch , float cGpa, float sGpa , String phone_number , String Email  ){
+    public Student(String student_name, String reg_no , String Section , String Batch , double cGpa, double sGpa , String phone_number , String Email  ){
 
             studentName = student_name;
+            section = Section;
             regNo = reg_no;
             batch = Batch;
             c_gpa = cGpa;
@@ -18,59 +22,48 @@ public class Student {
             email = Email;
 
     }
-    public void setEmail=(String email ){
-        String a ;
-        if( a = ".com" )
-            this.email = newEmail;
+
+    public void setEmail(String email) {
+        if ((this.email.substring(this.email.length() - 4, this.email.length()).equals(".com")))
+            this.email = email;
         else
-            System.out.println("");
+            System.out.println("Email must be valid.");
     }
-    public float getEmail() {
+    public String getEmail() {
         return this.email;
     }
-    public void setregNo(float newregno){
-        if(  )
-            this.regNo = newregNo;
-        else
-            System.out.println("CGPA can not be more than 4");
+    public void setRegNo(String newRegNo){
+            this.regNo = newRegNo;
     }
-    public float getC_gpa(){
+    public double getC_gpa() {
         return this.c_gpa;
-
-
-
-
-
-
-
-
+    }
 
     public void setC_gpa(float newCgpa){
-        if(newCgpa <= 4)
+        if(newCgpa <= 4 && newCgpa > 0)
             this.c_gpa = newCgpa;
         else
-            System.out.println("CGPA can not be more than 4");
+            System.out.println("CGPA can not be more than 4 or negative");
     }
-    public float getC_gpa(){
-        return this.c_gpa;
-    }
+
     public void setS_gpa(float newSgpa){
-        if(newSgpa <= 4)
+        if(newSgpa <= 4 && newSgpa > 0)
             this.s_gpa = newSgpa;
         else
-            System.out.println("SGPA can not be more than 4");
+            System.out.println("SGPA can not be more than 4 or negative");
     }
-    public float getS_gpa_gpa(){
+
+    public double getS_gpa(){
         return this.s_gpa;
     }
-    public void Batch(String newbatch){
-        if(length = 12)
-            this.batch  = newbatch;
-        else
-            System.out.println("CGPA can not be more than 4");
-    }
+
     public String getBatch(){
         return this.batch;
+    }
+
+    public static void main(String[] args) {
+        Student me = new Student("Wasif Ali", "FA21-BCS-035", "BCS-2A" ,"Fall-21", 3.73, 3.84, "0309-5348658", "waxif.alee1@gmail.com");
+        System.out.println(me.getEmail());
     }
 
 }
